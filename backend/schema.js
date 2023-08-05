59d4e333a91305e2f5b2900576b5b8d0f0e5603f
kwongster <bryant.j.kwong@gmail.com>
2023-08-05 16:06:13 -0700

    setup basic apollo client

--- backend/schema.js
+++ backend/schema.js
@@ -1,23 +1,22 @@
 export const typeDefs = `#graphql
-type Game {
+  type Game {
     id: ID!
     title: String!
     platform: [String!]!
-}
-type Review{
+  }
+  type Review {
     id: ID!
     rating: Int!
     content: String!
-}
-type Author{
+  }
+  type Author {
     id: ID!
     name: String!
     verified: Boolean!
-}
-type Query {
+  }
+  type Query {
     games: [Game]
     reviews: [Review]
     authors: [Author]
-}
-
+  }
 `;
